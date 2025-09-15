956f7b729d27974f2ed3393a4bfdcfedf426b727
Hugo Tamaki <hugotamaki@gmail.com>
2025-09-15 14:47:13 -0300

    Extracting event list to component

---
+++ src/components/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2 class="text-2xl font-medium">All Events</h2>
+  <section class="grid grid-cols-2 gap-8">
+    <template v-if="!eventsLoading">
+      <EventCard v-for="event in events" :key="event.id" :title="event.title" :when="event.date" :description="event.description" @register="$emit('register', event)"/>
+    </template>
+    <template v-else>
+      <LoadingEventCard v-for="i in 4" :key="i"/>
+    </template>
+  </section>
+</template>
+
+<script setup>
+  import { ref, onMounted } from 'vue';import EventCard from '@/components/EventCard.vue';
+  import LoadingEventCard from '@/components/LoadingEventCard.vue';
+
+  const events = ref([]);
+  const eventsLoading = ref(false);
+
+  const fetchEvents = async () => {
+    eventsLoading.value = true;
+    try {
+      const response = await fetch('http://localhost:3001/events');
+      events.value = await response.json();
+    } finally {
+      eventsLoading.value = false;
+    }
+  };
+
+  defineEmits(['register']);
+
+  onMounted(() => {
+    fetchEvents();
+  });
+</script>
